30b5b7ac2d54b36ef023dce9d9cbf92b42b67e5b
Ruttvik21 <rkhollam21@live.com>
2024-10-30 22:13:15 +0530

    initialise the board

--- src/app/api/initializeTheBoard/route.ts
+++ src/app/api/initializeTheBoard/route.ts
@@ -1,20 +1,20 @@
-import { GameModel } from '@/models/game';
-import { NextRequest } from 'next/server';
-import { connectToDatabase } from '../../../lib/mongodb';
+import { GameModel } from "@/models/game";
+import { NextRequest } from "next/server";
+import { connectToDatabase } from "../../../lib/mongodb";
 
 export async function GET(req: NextRequest) {
-    const { searchParams } = new URL(req.url);
-    const gameId = searchParams.get('gameId');
-    
+  const { searchParams } = new URL(req.url);
+  const gameId = searchParams.get("gameId");
+
   // Connect to MongoDB
   await connectToDatabase();
   const collection = GameModel;
-  
+
   const game = await collection.create({
     gameId,
-    board: Array(9).fill(''),
-    player: null,
-  })
+    board: Array(9).fill(""),
+    player: "X",
+  });
 
   return new Response(JSON.stringify(game.board), { status: 200 });
 }
